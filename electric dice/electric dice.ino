10e95d6e238865d2171c8e1bfb756cb8e8a1858c
Anthony Babka <anthonybabka@gmail.com>
2023-01-14 07:56:01 -0800

    first set up of code to test number generator and screen refresh

---
+++ electric dice/electric dice.ino
@@ -0,0 +1,67 @@
+#include <Adafruit_GFX.h>    // graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>             // Arduino library
+
+#define TFT_CS    10  // define chip select pin
+#define TFT_DC     9  // define data/command pin
+#define TFT_RST    8  // define reset pin, or set to -1 and connect to Arduino RESET pin
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+long randNumber;
+
+void setup() {
+  tft.init(240, 240, SPI_MODE2);
+
+  // if the screen is flipped, remove this command
+  tft.setRotation(2);
+
+  tft.fillScreen(0);
+  delay(100);
+  tft.setTextSize(6); //each 1 = 10 pixels
+  delay(2000);
+  tft.fillScreen(0);
+  delay(2000);
+  RandoNumber();
+  delay(5000);
+  tft.fillScreen(0);
+  RandoNumber2();
+  delay(5000);
+  tft.fillScreen(0);
+  delay(5000);
+  tft.fillScreen(0);
+}
+
+void loop() {
+}
+
+void RandoNumber() {
+    randomSeed(analogRead(0));
+    randNumber = random(1, 21);
+
+  tft.setTextSize(6);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print("Ready?");
+  delay(3000);
+  tft.fillScreen(0);
+  tft.setTextSize(10);
+  tft.setCursor(60, 60);
+  tft.setTextColor(ST77XX_WHITE);
+     tft.print(randNumber);
+}
+
+void RandoNumber2() {
+    randomSeed(analogRead(0));
+    randNumber = random(1, 20);
+
+  tft.setTextSize(6);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print("Again?");
+  delay(3000);
+  tft.fillScreen(0);
+  tft.setTextSize(10);
+  tft.setCursor(60, 60);
+  tft.setTextColor(ST77XX_BLUE);
+     tft.print(randNumber);
+}
